9fbf5b3ae59448a2f77d6a6a20bd605286db08c3
Venkata Nagendra Kumar Kone -X (vekone - CAPGEMINI AMERICA INC at Cisco) <vekone@partnet.cisco.com>
2021-07-09 13:25:52 +0530

    adding terraform files to create eks cluster

---
+++ security-groups.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "worker_group" {
+  name   = "worker_group_node"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    protocol    = "tcp"
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
